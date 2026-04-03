2968c00bf398d7ec93c22047d63aad6b604c950f
Zhao-Penghai <oceanytech@gmail.com>
2026-04-03 16:19:29 +0800

    260403

---
+++ bio/photos.js
@@ -0,0 +1,21 @@
+window.bioPhotos = [
+	{
+		src: 'bio/pic01.jpg',
+		alt: 'Christchurch, New Zealand',
+		date: 'June 17 2023',
+		location: 'Christchurch, New Zealand'
+	},
+	{
+		src: 'bio/pic02.jpg',
+		alt: 'Ulanqab, China',
+		date: 'April 4 2024',
+		location: 'Ulanqab, China'
+	},
+	{
+		src: 'bio/pic01.jpg',
+		alt: 'NTU, Singapore',
+		date: 'June 17 2023',
+		location: 'NTU, Singapore'
+	},
+
+];
